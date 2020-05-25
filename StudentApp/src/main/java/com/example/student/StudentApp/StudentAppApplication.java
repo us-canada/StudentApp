2bc8a55ad4c9ea801b66e34e051ed501703bd99b
Surbhi Sharma <surbhimishra03@gmail.com>
2020-05-24 23:51:05 -0500

    New App

--- StudentApp/src/main/java/com/example/student/StudentApp/StudentAppApplication.java
+++ StudentApp/src/main/java/com/example/student/StudentApp/StudentAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentAppApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentAppApplication.class, args);
 	}
